2f36b5dbb769583d71495203238381b838a87c05
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-06-29 18:07:12 +0200

    Update functionsWordPress.php

--- functionsWordPress.php
+++ functionsWordPress.php
@@ -48,6 +48,25 @@ function finalizar () {
 	//echo 'Page generated in '.$total_time.' seconds.';
 }
 
+// Permite embeber codigo PHP en el cuerpo de los post y las paginas de wordpress
+//	ejemplo: [insert_php] echo adrotate_group(9); [/insert_php]
+add_filter( 'the_content', 'exectPHP_content', 9 );
+function exectPHP_content($content)
+{
+	$will_bontrager_content = $content;
+	preg_match_all('!\[insert_php[^\]]*\](.*?)\[/insert_php[^\]]*\]!is',$will_bontrager_content,$will_bontrager_matches);
+	$will_bontrager_nummatches = count($will_bontrager_matches[0]);
+	for( $will_bontrager_i=0; $will_bontrager_i<$will_bontrager_nummatches; $will_bontrager_i++ )
+	{
+		ob_start();
+		eval($will_bontrager_matches[1][$will_bontrager_i]);
+		$will_bontrager_replacement = ob_get_contents();
+		ob_clean();
+		ob_end_flush();
+		$will_bontrager_content = preg_replace('/'.preg_quote($will_bontrager_matches[0][$will_bontrager_i],'/').'/',$will_bontrager_replacement,$will_bontrager_content,1);
+	}
+	return $will_bontrager_content;
+} # function will_bontrager_insert_php()
 
 ?>
 
