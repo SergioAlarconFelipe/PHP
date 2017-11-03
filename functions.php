ce990edf87cdf6b2a2fa66eaf0d977e3410ebff7
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-11-03 08:25:08 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,15 +22,69 @@ echo 'length = ' . strlen( $c ) . " | string = " . $c;
 
 
 //Sanear strings
-function normaliza ($cadena){
-    $originales = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞ
+function sanear_string1($string){
+	$string = trim($string);
+ 
+	$string = str_replace(
+		array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+		array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+		$string
+	);
+ 
+	$string = str_replace(
+		array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+		array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+		$string
+	);
+ 
+	$string = str_replace(
+		array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+		array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+		$string
+	);
+ 
+	$string = str_replace(
+		array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+		array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+		$string
+	);
+ 
+	$string = str_replace(
+		array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+		array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+		$string
+	);
+ 
+	$string = str_replace(
+		array('ñ', 'Ñ', 'ç', 'Ç'),
+		array('n', 'N', 'c', 'C',),
+		$string
+	);
+ 
+	//Esta parte se encarga de eliminar cualquier caracter extraño
+	$string = str_replace(
+		array("", "¨", "º", "-", "~", "#", "@", "|", "!",
+			 "·", "$", "%", "&", "",
+			 "(", ")", "?", "'", "¡",
+			 "¿", "[", "^", "<code>", "]",
+			 "+", "}", "{", "¨", "´",
+			 ">", "< ", ";", ",", ":",
+			 ".", " "),
+		'',
+		$string
+	);
+	
+	return $string;
+}
+function sanear_string2 ($cadena){
+	$originales = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞ
 ßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕ';
-    $modificadas = 'aaaaaaaceeeeiiiidnoooooouuuuy
+	$modificadas = 'aaaaaaaceeeeiiiidnoooooouuuuy
 bsaaaaaaaceeeeiiiidnoooooouuuyybyRr';
-    $cadena = utf8_decode($cadena);
-    $cadena = strtr($cadena, utf8_decode($originales), $modificadas);
-    $cadena = strtolower($cadena);
-    return utf8_encode($cadena);
+	$cadena = utf8_decode($cadena);
+	$cadena = strtr($cadena, utf8_decode($originales), $modificadas);
+	$cadena = strtolower($cadena);
+	return utf8_encode($cadena);
 }
 
 
