05bf196e875d184200b9d220300287b4cd39d294
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2018-04-26 15:30:13 +0200

    Create function.php

---
+++ wol/function.php
@@ -0,0 +1,39 @@
+<?php
+function wol( $broadcast, $mac ) {
+    $mac_array = split( ':', $mac );
+	
+    $hwaddr = '';
+
+    foreach( $mac_array AS $octet ) {
+        $hwaddr .= chr( hexdec( $octet ) );
+    }
+	
+	// Create Magic Packet
+
+    $packet = '';
+    for ($i = 1; $i <= 6; $i++) {
+        $packet .= chr(255);
+    }
+
+    for ($i = 1; $i <= 16; $i++) {
+        $packet .= $hwaddr;
+    }
+	
+	var_dump( $packet );
+
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+	
+    if( $sock ) {
+        $options = socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, true);
+
+        if( $options >= 0 ) {    
+            $e = socket_sendto( $sock, $packet, strlen( $packet ), 0, $broadcast, 4343 );
+			
+            socket_close( $sock );
+        }    
+    }
+}
+
+//wol( 'sdz.ddns.net', '00:18:F3:99:ed:20' );
+wol( '90.77.55.216', '00:18:F3:99:ed:20' );
+?>
