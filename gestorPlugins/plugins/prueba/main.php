1999daa97e96e1444c418668471db47059bcf835
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2019-11-27 17:55:37 +0100

    Update main.php

--- gestorPlugins/plugins/prueba/main.php
+++ gestorPlugins/plugins/prueba/main.php
@@ -1,18 +1,39 @@
 <?php
+
+// funciones
 function p1_1() {
-	echo 'hola: <br/>';
+	echo 'p1_1 => hola: <br/>';
 }
 function p1_2( $param1 = '' ) {
-	echo 'hola: ' . $param1 . '<br/>';
+	echo 'p1_2 => hola: ' . print_r( $param1, true ) . '<br/>';
 }
 function p1_3( $param1 = '', $param2 = '', $param3 = '' ) {
-	echo 'hola: ' . $param1 . ', ' . $param2 . ', ' . $param3 . '<br/>';
+	echo 'p1_3 => hola: ' . print_r( $param1, true ) . ', ' . print_r( $param2, true ) . ', ' . print_r( $param3, true ) . '<br/>';
 }
 addAction( 'body', 'p1_1', 4 );
 addAction( 'body', 'p1_2', 2 );
 addAction( 'body', 'p1_3', 6 );
 
+// metodos de objeto
 include 'Dato.php';
 $d = new Dato(8);
 addAction( 'body', array( $d, 'echoValor' ), -9 );
 addAction( 'body', array( $d, 'echoSumarValor' ), -15 );
+addAction( 'body', array( $d, 'echoValor' ), -10 );
+
+// metodos de objeto con parametros
+$d = new Dato(8);
+
+addAction( 'footer2', array( $d, 'echoValor' ) );
+addAction( 'footer2', array( $d, 'setValor' ) );
+addAction( 'footer2', array( $d, 'echoValor' ) );
+
+
+
+function p1_4( $param1 = '', $param2 = '', $param3 = '' ) {
+	echo 'p1_4<br/>';
+	var_dump( $param1 );
+	var_dump( $param2 );
+	var_dump( $param3 );
+}
+addAction( 'body', 'p1_4' );
