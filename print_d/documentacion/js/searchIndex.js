53d77e8b2b4f4c8bf6f221b7da0a266a5607ecbf
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2021-05-11 08:19:26 +0200

    1.3.0

---
+++ print_d/documentacion/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\encode_utf8_ansi\u0028\u0029",
+            "name": "encode_utf8_ansi",
+            "summary": "Funcion\u0020que\u0020convierte\u0020los\u0020caracteres\u0020Unicode\u0020en\u0020ASCII\u0020\u0028UTF8\u0029.",
+            "url": "../namespaces/default.html#function_encode_utf8_ansi"
+        },                {
+            "fqsen": "\\print_d_StylesScripts\u0028\u0029",
+            "name": "print_d_StylesScripts",
+            "summary": "Funcion\u0020para\u0020la\u0020insercion\u0020de\u0020los\u0020CSS\u0020y\u0020JS\u0020necesarios\u0020en\u0020la\u0020depuracion\u0020de\u0020variables.",
+            "url": "../namespaces/default.html#function_print_d_StylesScripts"
+        },                {
+            "fqsen": "\\print_d_options\u0028\u0029",
+            "name": "print_d_options",
+            "summary": "Funcion\u0020depuracion\u0020de\u0020variables.",
+            "url": "../namespaces/default.html#function_print_d_options"
+        },                {
+            "fqsen": "\\print_d\u0028\u0029",
+            "name": "print_d",
+            "summary": "Funcion\u0020para\u0020la\u0020depuracion\u0020de\u0020variables\u0020en\u0020grupo.",
+            "url": "../namespaces/default.html#function_print_d"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "../namespaces/default.html"
+        }            ]
+);
